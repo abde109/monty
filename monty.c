efd6e6b23f3df88de362db752b8134a7e1164321
abde109 <abderrahimkhadri2@gmail.com>
2023-08-23 20:55:00 +0100

    task abderrahim status

--- monty.c
+++ monty.c
@@ -76,6 +76,26 @@ int main(int argc, char *argv[])
         }
         else if (strcmp(opcode, "pall") == 0)
             pall(&stack, line_number);
+        else if (strcmp(opcode, "pint") == 0)
+        {
+            pint(&stack, line_number);
+        }
+        else if (strcmp(opcode, "pop") == 0)
+        {
+            pop(&stack, line_number);
+        }
+        else if (strcmp(opcode, "swap") == 0)
+        {
+            swap(&stack, line_number);
+        }
+        else if (strcmp(opcode, "add") == 0)
+        {
+            add(&stack, line_number);
+        }
+        else if (strcmp(opcode, "pop") == 0)
+        {
+            pop(&stack, line_number);
+        }
         else
         {
             fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
